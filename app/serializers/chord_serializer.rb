76e1f6f03fe245278304bef375b4cad3d7151821
Marat Oshmiansky <maratical@gmail.com>
2021-08-17 13:57:26 -0700

    add associations to serializers

--- app/serializers/chord_serializer.rb
+++ app/serializers/chord_serializer.rb
@@ -1,3 +1,6 @@
 class ChordSerializer < ActiveModel::Serializer
   attributes :id, :name, :image, :chord_root, :alteration, :quality, :option
+
+  has_many :user_chords
+  has_many :users, through: :user_chords
 end
